50628b29849abeff3e28276d430c7fb82013431b
Marei Kikukawa <contact@calmery.me>
2018-03-02 01:46:59 +0900

    Add function to divide slide

--- src/elm/Slides.elm
+++ src/elm/Slides.elm
@@ -1,7 +1,19 @@
 module Slides exposing (slides)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Markdown exposing (toHtml)
+
+
+-- Support
+
+
+divide : List (Html msg) -> List (Html msg) -> Html msg
+divide left right =
+    div [ style [ ( "display", "flex" ) ] ]
+        [ div [ style [ ( "width", "50%" ) ] ] left
+        , div [ style [ ( "width", "50%" ) ] ] right
+        ]
 
 
 slides : List (Html msg)
@@ -13,4 +25,8 @@ slides =
 # Second
 """
     , h1 [] [ text "Third" ]
+    , div []
+        [ h1 [] [ text "Fourth" ]
+        , divide [ text "Left" ] [ text "Right" ]
+        ]
     ]
