d29585c99b595eaa59dfa6a1e21d132227a82e20
Marei Kikukawa <contact@calmery.me>
2018-03-04 23:46:16 +0900

    Add seek bar

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -2,6 +2,7 @@ module View exposing (view)
 
 import Html exposing (Html, Attribute, div, text)
 import Html.Attributes exposing (style)
+import List exposing (length)
 import List.Extra exposing ((!!))
 import Model exposing (Model, Route(Slide))
 import Update exposing (Msg)
@@ -24,17 +25,53 @@ slideStyle =
         ]
 
 
+seekBar : List (Html Msg) -> Route -> Html Msg
+seekBar slides currentPosition =
+    case currentPosition of
+        Slide position ->
+            let
+                numberOfSlides =
+                    length slides
+
+                percent =
+                    (toFloat (position + 1)) * ((toFloat 100) / (toFloat numberOfSlides))
+            in
+                div
+                    [ style
+                        [ ( "width", "100%" )
+                        , ( "height", "5px" )
+                        , ( "top", "0" )
+                        , ( "left", "0" )
+                        , ( "position", "absolute" )
+                        , ( "background", "#FFF" )
+                        , ( "border-bottom", "1px solid #E6E6E6" )
+                        ]
+                    ]
+                    [ div
+                        [ style
+                            [ ( "width", (toString percent) ++ "%" )
+                            , ( "height", "5px" )
+                            , ( "background", "#E6E6E6" )
+                            ]
+                        ]
+                        []
+                    ]
+
+
 view : Model -> Html Msg
 view model =
-    div [ slideStyle ]
-        [ div [ style [ ( "padding", "29.5px 68.5px" ) ] ]
-            [ case model.currentPosition of
-                Slide currentPosition ->
-                    case slides !! currentPosition of
-                        Just slide ->
-                            slide
+    div []
+        [ seekBar slides model.currentPosition
+        , div [ slideStyle ]
+            [ div [ style [ ( "padding", "29.5px 68.5px" ) ] ]
+                [ case model.currentPosition of
+                    Slide currentPosition ->
+                        case slides !! currentPosition of
+                            Just slide ->
+                                slide
 
-                        Nothing ->
-                            text ""
+                            Nothing ->
+                                text ""
+                ]
             ]
         ]
